fe13b5ef5c474847e7fb5b4c7586d3a62a649ad2
Assidiqi <vassidiqi@gmail.com>
2021-03-11 17:34:11 +0100

    update menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
+import { Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+/* const HomePage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Link to='/topics'>TOPICS</Link>
+      <br/>
+      <button onClick={() => props.history.push('/topics')}>To Topics</button>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+const TopicsList = (props) => (
+  <div>
+    <h1>Topic List Page</h1>
+    <Link to={`${props.match.url}/13`}>TO TOPICS 13</Link>
+    <Link to={`${props.match.url}/115`}>TO TOPICS 115</Link>
+    <Link to={`${props.match.url}/53`}>TO TOPICS 53</Link>
+  </div>
+)
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic Detail Page: {props.match.params.topicID}</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/topics' component={TopicsList} />
+        <Route path='/topics/:topicID' component={TopicDetail} />
+      </Switch>
+
     </div>
   );
+} */
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
